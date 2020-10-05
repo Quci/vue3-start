ed955c72accd79250da4349a8bac6618f03fd238
Quci <xujiejiuxi@gmail.com>
2020-10-05 21:14:48 +0800

    test

---
+++ src/views/setup.vue
@@ -0,0 +1,31 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import {ref, reactive} from 'vue';
+
+export default {
+    props: {
+        name: {
+            type: String,
+            default: 'Neo',
+        },
+    },
+    setup(props) {
+        const count = ref(0);
+        const object = reactive({foo: 'bar'});
+        console.log(count);
+        console.log(props);
+        console.log(object);
+
+        return {
+            count,
+            object,
+        }
+
+    },
+
+
+}
+</script>
