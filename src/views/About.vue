ae0cd59966ef4e1b0daaa5d09c65ad52ea63868c
Quci <xujiejiuxi@gamil.com>
2020-10-09 13:19:50 +0800

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div>
+        <div ref="el">{{ data.name }}</div>
+        <button @click="handleClick">click</button>
+        <div>{{count}}</div>
+    </div>
 </template>
+<script>
+import useLogic from "@/common/useLogic";
+
+export default {
+    name: 'some component',
+    setup() {
+        const {data, el, count, handleClick} = useLogic()
+
+
+        return {
+            data,
+            el,
+            count,
+            handleClick
+        }
+    }
+}
+</script>
+
+<style scoped lang='scss'>
+
+</style>
