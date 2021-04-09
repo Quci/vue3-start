d6f657b09633d06e83e2d11aecd920ee993337bd
Quci <xujiejiuxi@gmail.com>
2021-04-09 15:02:58 +0800

    with jsx

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
+  "plugins": ["@vue/babel-plugin-jsx"],
   presets: [
-    "vca-jsx",
+    // "vca-jsx",
     '@vue/cli-plugin-babel/preset',
   ]
 }
--- src/views/CustomRef.vue
+++ src/views/CustomRef.vue
@@ -1,39 +1,69 @@
 <template>
-    <div>
-        <label>
-            <input type="text" v-model="foo">
-        </label>
-    </div>
+  <div>
+    <Dsd></Dsd>
+    <label>
+      <input type="text" v-model="foo" @input="handleInput">
+      {{ foo }}
+    </label>
+  </div>
 </template>
 
 <script>
 import {customRef} from 'vue'
+import Dsd from "@/components/Dsd";
 
-function useDebouncedRef(value, delay) {
-    return customRef((track, trigger) => {
-        return {
-            get() {
-                track();
-                console.log('get value: ', value);
-                return value;
-            },
-            set(newValue, oldValue) {
-                console.log('newValue', newValue);
-                value = newValue;
-                trigger();
-            }
-        }
-    })
+function useDebouncedRef(value, delay = 2000) {
+  let timeout
+  return customRef((track, trigger) => {
+    return {
+      get() {
+        track()
+        return value
+      },
+      set(newValue) {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+          console.log(111);
+          value = newValue
+          trigger()
+        }, delay)
+      }
+    }
+  })
 }
 
+// function useDebouncedRef(value, delay) {
+//     return customRef((track, trigger) => {
+//         return {
+//             get() {
+//                 track();
+//                 console.log('get value: ', value);
+//                 return value;
+//             },
+//             set(newValue, oldValue) {
+//                 console.log('newValue', newValue);
+//                 value = newValue;
+//                 trigger();
+//             }
+//         }
+//     })
+// }
+
 export default {
-    name: "CustomRef",
-    setup() {
-        const foo = useDebouncedRef('c.c');
-        return {
-            foo
-        }
-    },
+  name: "CustomRef",
+  components: {Dsd},
+  setup() {
+    const foo = useDebouncedRef('');
+
+    function handleInput(val) {
+      console.log('222', val);
+    }
+
+    return {
+      foo,
+      handleInput
+    }
+  }
 }
 
 </script>
